ecf28623f863cda966249d96846441d31db746cb
Nguyễn Vũ Khoa <150508398+Nvkhoaaa@users.noreply.github.com>
2025-12-10 23:01:04 +0700

    Update Utils

---
+++ utils/jwt.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const SECRET_KEY = process.env.SECRET_KEY;
+const jwt = require('jsonwebtoken');
+
+function generateToken(payload) {
+    return jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
+}
+
+function verifyToken(token) {
+    try {
+        const decoded = jwt.verify(token, SECRET_KEY);
+        return { valid: true, decoded };
+    } catch (err) {
+        return { valid: false, error: err };
+    }
+}
+
+module.exports = { generateToken, verifyToken };
